e5d4f3420ed5019356efd34f6701f7677edae874
guillaume barillot <gbarillot@gmail.com>
2022-01-08 22:00:11 +0100

    Implement contact form

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,20 @@
 save = function() {
-  alert('here')
+  var data = new FormData();
+  data.append("email", document.getElementById("input-1").value);
+  data.append("message", document.getElementById("input-2").value);
+ 
+  fetch("https://app.99inbound.com/api/e/Ap4nFHvh", {
+    method: "POST",
+    body: data
+  }).then((result) => {
+    if (result.status != 200) { 
+      alert("Une errur est survenue"); 
+    }
+  }).then((response) => {
+    document.getElementById("input-1").value = '';
+    document.getElementById("input-2").value = '';
+    setTimeout(function() {
+      alert("J'ai bien reçu votre message, je vous répondrai dès que possible !")
+    }, 0)
+  }) 
 }
